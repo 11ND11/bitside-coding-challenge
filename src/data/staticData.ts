4d2b7198fbca81b7f751616e23c8e62af81f5498
asteiger <andreas.steiger@tritum.de>
2025-01-08 21:02:43 +0100

    [TASK] Add data of inventory and promotions

---
+++ src/data/staticData.ts
@@ -0,0 +1,12 @@
+export const inventoryData = [
+    { "sku": "A0001", "name": "Artikel A0001", "price": 12.99 },
+    { "sku": "A0002", "name": "Artikel A0002", "price": 3.99 },
+    { "sku": "A0003", "name": "Artikel A0003", "price": 10.00 },
+    { "sku": "A0004", "name": "Artikel A0004", "price": 20.00 }
+];
+
+export const promotionsData = [
+    { "sku": "A0002", "type": "buyOneGetOneFree" },
+    { "sku": "A0001", "type": "tenPercentOff" },
+    {"sku": "A0003", "type": "tenPercentOff" }
+];
